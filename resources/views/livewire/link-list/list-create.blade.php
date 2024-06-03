fe040c12b0ae604254eec1fee166da63b0e1dea3
CesarObedFL <cesarobedfl@gmail.com>
2024-06-03 16:37:59 -0600

    [ADD] saving span to forms

--- resources/views/livewire/link-list/list-create.blade.php
+++ resources/views/livewire/link-list/list-create.blade.php
@@ -26,6 +26,8 @@
                 <button type="submit" data-modal-hide="default-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Create </button>
                 <button wire:click="close" type="button" data-modal-hide="default-modal" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"> Cancel </button>
             </div>
+
+            <span wire:loading>Saving...</span> 
         </form>
     </div>
 
