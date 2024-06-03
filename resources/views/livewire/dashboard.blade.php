e19b8e877d050afa7754613c4eb4f728d0ddadb8
CesarObedFL <cesarobedfl@gmail.com>
2024-06-03 13:26:55 -0600

    [FIX] dashboard layout

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -15,9 +15,9 @@
                     <div class="grid grid-cols-6 gap-2">
                         @foreach( $lists as $list )
                         <br>
-                        <div class="col-start-2 col-end-7">
-                            <a href="{{ route('link-list', [ 'id' => $list->id ]) }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-90 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
+                        <div class="col-start-2 col-span-4">
+                            <a href="{{ route('link-list', [ 'id' => $list->id ]) }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-60 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="">
                                 <div class="flex flex-col justify-between p-4 leading-normal">
                                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $list->name }}</h5>
                                     <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> -->
