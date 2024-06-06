666205ab3626a5a7c1cf54c9835714e200d94fe6
CesarObedFL <cesarobedfl@gmail.com>
2024-06-06 12:38:56 -0600

    [ADD] flowbite cdn's and colored errors on create list form

--- resources/views/livewire/link-list/list-create.blade.php
+++ resources/views/livewire/link-list/list-create.blade.php
@@ -19,15 +19,15 @@
             <div class="p-4 md:p-5 space-y-4">
                 <div class="col-span-2">
                     <label for="url" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">List Name</label>
-                    <input wire:model="list_name" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the name of the list to create..." required="">
-                    <div>@error('list_name') {{ $message }} @enderror</div>
+                    <input wire:model="list_name" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the name of the list to create...">
+                    <div style="color: red;">@error('list_name') {{ $message }} @enderror</div>
                 </div>
             </div>
             <div class="p-4 md:p-5 space-y-4">
                 <div class="col-span-2">
                     <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
                     <textarea wire:model="list_description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write the description here..."></textarea>
-                    <div>@error('list_description') {{ $message }} @enderror</div>
+                    <div style="color: red;">@error('list_description') {{ $message }} @enderror</div>
                 </div>
             </div>
             <div class="p-4 md:p-5 space-y-4">
@@ -35,7 +35,7 @@
                     <label for="list_image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description Image</label>
                     <input wire:model="list_image" type="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
                     <div wire:loading wire:target="list_image">Loading image...</div>
-                    @error('list_image') <span class="error">{{ $message }}</span> @enderror
+                    @error('list_image') <span class="error" style="color: red;">{{ $message }}</span> @enderror
                 </div>
             </div>
 
