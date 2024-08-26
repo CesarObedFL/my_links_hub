f38e541b198ce3534f18f20cbe2e7fefe10bdec3
CesarObedFL <cesarobedfl@gmail.com>
2024-08-26 15:47:41 -0600

    [ADD] article card to dashboard lists

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,7 +1,7 @@
 <div>
     <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-slate-800 leading-tight">
                 {{ __('Dashboard') }}
             </h2>
         </div>
@@ -9,45 +9,45 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg ">
+            <div class="overflow-hidden sm:rounded-lg">
 
-                    @if ( count($lists) > 0 )
-                        <div class="grid grid-cols-6 gap-2">
-                            @foreach( $lists as $list )
-                            <br>
-                            <div class="col-start-2 col-span-4">
-                                <a href="{{ route('link-list', [ 'id' => $list->id ]) }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-60 md:rounded-none md:rounded-s-lg" src="{{ asset('list_images/'.$list->list_image) }}" alt="{{ $list->list_image }}">
-                                    <div class="flex flex-col justify-between p-4 leading-normal">
-                                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $list->list_name }}</h5>
-                                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $list->list_description }}</p>
-                                    </div>
-                                </a>
+                @if ( count($lists) > 0 )
+                    <div class="grid gap-4 md:grid-cols-3 md:gap-6">
+                        @foreach( $lists as $list )
+                        <article class="bg-white h-200 w-130 flex flex-col shadow-lg rounded overflow-hidden">
+                            <div class="h-60">
+                                <img src="{{ asset('list_images/'.$list->list_image) }}" alt="{{ $list->list_image }}" class="h-full w-full object-cover object-center">
                             </div>
-                            @endforeach
-                        </div>  
-                        <br>
-                    @else
-                        <br>
-                        <div id="alert-4" class="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
-                            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                            </svg>
-                            <span class="sr-only">Info</span>
-                            <div class="ms-3 text-sm font-medium">
-                                There is no registered list
+                            <div class="p-5 space-y-3 flex-1">
+                                <h3 class="text-sm font-semibold text-slate-400">{{ $list->list_name }}</h3>
+                                <h2 class="text-md font-semibold text-slate-800 leading-tight">{{ $list->list_description }}</h2>
                             </div>
-                            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-yellow-50 text-yellow-500 rounded-lg focus:ring-2 focus:ring-yellow-400 p-1.5 hover:bg-yellow-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-yellow-300 dark:hover:bg-gray-700" data-dismiss-target="#alert-4" aria-label="Close">
-                                <span class="sr-only">Close</span>
-                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                </svg>
-                            </button>
+                            <div class="flex space-x-2 p-5">
+                                <a href="{{ route('link-list', [ 'id' => $list->id ]) }}">Go</a>
+                            </div>
+                        </article>
+                        @endforeach
+                    </div>
+                    <br>
+                @else
+                    <br>
+                    <div id="alert-4" class="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+                        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        <span class="sr-only">Info</span>
+                        <div class="ms-3 text-sm font-medium">
+                            There is no registered list
                         </div>
-                    @endif
+                        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-yellow-50 text-yellow-500 rounded-lg focus:ring-2 focus:ring-yellow-400 p-1.5 hover:bg-yellow-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-yellow-300 dark:hover:bg-gray-700" data-dismiss-target="#alert-4" aria-label="Close">
+                            <span class="sr-only">Close</span>
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                        </button>
+                    </div>
+                @endif
 
-                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/link-list/link-listing.blade.php
+++ resources/views/livewire/link-list/link-listing.blade.php
@@ -1,7 +1,7 @@
 <div>
 
     <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ $list_name->list_name }}
             </h2>
