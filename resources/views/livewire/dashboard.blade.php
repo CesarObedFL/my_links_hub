04aae1952257b6ff24d04768a3af5fa08968652c
CesarObedFL <cesarobedfl@gmail.com>
2024-08-26 18:06:19 -0600

    [ADD] article card list: go_link_icon

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -23,7 +23,15 @@
                                 <h2 class="text-md font-semibold text-slate-800 leading-tight">{{ $list->list_description }}</h2>
                             </div>
                             <div class="flex space-x-2 p-5">
-                                <a href="{{ route('link-list', [ 'id' => $list->id ]) }}">Go</a>
+                                <a href="{{ route('link-list', [ 'id' => $list->id ]) }}" class="bg-white text-sm h-6 w-10 rounded">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 fill-current text-slate-400 duration-300 hover:rotate-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                                    </svg>
+                                </a>
+                                <button class="">
+                                    
+
+                                </button>
                             </div>
                         </article>
                         @endforeach
