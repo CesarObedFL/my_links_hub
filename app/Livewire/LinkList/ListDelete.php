99ee86f749ab16d37cd145b07c88b08d438c087a
CesarObedFL <cesarobedfl@gmail.com>
2024-08-28 16:02:47 -0600

    [ADD] ListDelete function

--- app/Livewire/LinkList/ListDelete.php
+++ app/Livewire/LinkList/ListDelete.php
@@ -4,14 +4,24 @@ namespace App\Livewire\LinkList;
 
 use App\Http\Livewire\Auth;
 use LivewireUI\Modal\ModalComponent;
+use Illuminate\Support\Facades\Storage;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use App\Models\LinkList;
+use App\Models\Link;
 
 class ListDelete extends ModalComponent
 {
     use LivewireAlert;
 
     public $list_id;
+    public $list_name;
+    public $question_response;
+
+    public function mount()
+    {
+        $list = LinkList::where('id', $this->list_id)->first();
+        $this->list_name = $list->list_name;
+    }
 
     public function render()
     {
@@ -20,14 +30,29 @@ class ListDelete extends ModalComponent
 
     public function delete_list()
     {
+        $list = LinkList::where('id', $this->list_id)->first();
+        $links = Link::where('link_list_id', $this->list_id)->get();
 
+        foreach( $links as $link ) {
+            if ( $link->thumbnail ) {
+                Storage::disk('public_thumbnails')->delete($link->thumbnail);
+            }
+        }
+
+        $list->delete(); 
         $this->dispatch('list_deleted');
         $this->close();
-        //$this->alert('success', 'List deleted successfully!...', [ 'position' => 'center', 'timer' => 2500 ]);
+    }
+
+    public function reset_fields()
+    {
+        $this->list_id = '';
+        $this->list_name = '';
     }
 
     public function close()
     {
+        $this->reset_fields();
         $this->closeModal();
     }
 
--- resources/views/livewire/link-list/list-delete.blade.php
+++ resources/views/livewire/link-list/list-delete.blade.php
@@ -2,8 +2,8 @@
 
     <div class="relative p-4 w-full max-w-2xl">
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-            <h3 class="text-xl font-semibold text-gray-900">
-                Delete List
+            <h3 class="text-xl font-semibold text-slate-800 uppercase">
+                delete list
             </h3>
             <button wire:click="close" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -16,9 +16,11 @@
         <form wire:submit.prevent="delete_list" action="#" method="POST" role="form">
             @csrf
 
+            <h2 class="p-4">Are you sure to eliminate <span class="">{{ $list_name }} list</span>?</h2>
+            <p>You are going to remove all of the links stored in this list</p>
             <!-- Modal footer -->
             <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button type="submit" data-modal-hide="default-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Create </button>
+                <button type="submit" data-modal-hide="default-modal" class="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"> Yes </button>
                 <button wire:click="close" type="button" data-modal-hide="default-modal" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"> Cancel </button>
             </div>
 
