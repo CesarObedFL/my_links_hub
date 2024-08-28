09fb60857aa2607fc435c92152c6a2a8143e2474
CesarObedFL <cesarobedfl@gmail.com>
2024-08-28 16:59:55 -0600

    [ADD] list-delete layout

--- resources/views/livewire/link-list/list-delete.blade.php
+++ resources/views/livewire/link-list/list-delete.blade.php
@@ -5,7 +5,7 @@
             <h3 class="text-xl font-semibold text-slate-800 uppercase">
                 delete list
             </h3>
-            <button wire:click="close" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+            <button wire:click="close" type="button" class="text-slate-600 bg-transparent duration-300 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -16,12 +16,19 @@
         <form wire:submit.prevent="delete_list" action="#" method="POST" role="form">
             @csrf
 
-            <h2 class="p-4">Are you sure to eliminate <span class="">{{ $list_name }} list</span>?</h2>
-            <p>You are going to remove all of the links stored in this list</p>
+            <h2 class="text-lg p-4 flex gap-2">
+                </span class="my-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-yellow-400 duration-300 hover:text-yellow-800 hover:scale-125">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                    </svg>
+                </span>
+                Are you sure to eliminate <i>{{ $list_name }}</i> list?
+            </h2>
+            <p class="p-4">You are going to remove all of the links stored in this list</p>
             <!-- Modal footer -->
-            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button type="submit" data-modal-hide="default-modal" class="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"> Yes </button>
-                <button wire:click="close" type="button" data-modal-hide="default-modal" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"> Cancel </button>
+            <div class="flex items-center p-4 md:p-5 border-t border-slate-300 rounded-b">
+                <button type="submit" data-modal-hide="default-modal" class="bg-green-300 text-white py-2.5 px-5 text-sm text-center font-medium rounded-lg duration-300 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-green-600"> Yes </button>
+                <button wire:click="close" type="button" data-modal-hide="default-modal" class="bg-red-300 text-white py-2.5 px-5 ms-3 text-sm font-medium border rounded-lg duration-300 hover:bg-red-600 focus:z-10 focus:outline-none focus:ring-4 focus:ring-red-600"> Cancel </button>
             </div>
 
             <span wire:loading>Loading...</span> 
