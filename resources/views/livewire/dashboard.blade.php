185aa0ca537d35eac422cda6e5d9e076dffa669f
CesarObedFL <cesarobedfl@gmail.com>
2024-08-27 18:56:05 -0600

    [FIX] dashboard serach input

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -6,14 +6,14 @@
                     {{ __('Dashboard') }}
                 </h2>
             </div>
-<div class="flex justify-end">
-  <div class="relative w-36 md:w-96">
-    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-      <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-    </div>
-    <input type="text" id="search" class="block w-full pl-10 text-sm text-gray-900 bg-white border border-slate-400 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar...">
-  </div>
-</div>
+            <div class="flex justify-end">
+                <div class="relative w-36 md:w-96">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    </div>
+                    <input type="text" id="search" class="block w-full pl-10 text-sm text-gray-900 bg-white border border-slate-400 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar...">
+                </div>
+            </div>
         </div>
     </header>
 
