ed4c93cf8dc5d714d5f6cd9dfd5ec7ba2041f62c
CesarObedFL <cesarobedfl@gmail.com>
2024-08-30 13:18:21 -0600

    [ADD] profile; header style

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+        <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Profile') }}
+            </h2>
+        </div>
     </x-slot>
 
     <div>
