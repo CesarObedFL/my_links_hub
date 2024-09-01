bbdbfa6a14293a21c2c138732e754edb0ec9b1d1
CesarObedFL <cesarobedfl@gmail.com>
2024-08-31 19:53:35 -0600

    [FIX] dashboard responsive px-6

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -17,7 +17,7 @@
         </div>
     </header>
 
-    <div class="py-12">
+    <div class="py-12 px-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden sm:rounded-lg">
 
