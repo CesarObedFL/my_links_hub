c93bc664883d55aa085978fbce44a77d6847f39e
CesarObedFL <cesarobedfl@gmail.com>
2024-09-03 17:24:57 -0600

    [ADD] list-listing responsive md, lg y xl screen sizes

--- resources/views/livewire/link-list/link-listing.blade.php
+++ resources/views/livewire/link-list/link-listing.blade.php
@@ -13,18 +13,19 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
 
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <!-- md, lg xl screen size table -->
+                    <table class="w-full hidden sm:block text-sm text-left table-auto rtl:text-right text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-sky-200">
                             <tr>
-                                <th colspan="2" scope="colgroup" class="px-6 py-3"> 
-                                    <select wire:model.live="per_page" class="block w-full p-2 mt-6 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
+                                <th colspan="1" scope="colgroup" class="px-2 py-3"> 
+                                    <select wire:model.live="per_page"  class="block w-full p-2 mt-6 mb-6 text-sm text-slate-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
                                         <option value="10">10 per page</option>
                                         <option value="25">25 per page</option>
                                         <option value="50">50 per page</option>
                                     </select>
                                 </th>
-                                <th colspan="3" scope="colgroup" class="px-6 py-3"> 
-                                    <input wire:model.live="search" type="search" placeholder="search by..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                <th colspan="4" scope="colgroup" class="px-6 py-3"> 
+                                    <input wire:model.live="search" type="search" placeholder="search by..." class="block w-full p-2.5 text-sm text-slate-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
                                 </th>
                             </tr>
                             <tr>
@@ -105,31 +106,31 @@
                                         @if ( $link->thumbnail )
                                             <img src="{{ asset('thumbnails/'.$link->thumbnail) }}" class="w-16 md:w-32 max-w-full max-h-full" alt="{{ $link->thumbnail }}">
                                         @else 
-                                            <img src="{{ asset('images/app/default_image.png') }}" class="w-20 md:w-40 max-w-full max-h-full" alt="default image">
+                                            <img src="{{ asset('images/app/default_image.png') }}" class="w-16 md:w-32 max-w-full max-h-full" alt="default image">
                                         @endif
                                     </td>
-                                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap" >
+                                    <td class="px-6 py-4 text-xs lg:text-base text-gray-900 whitespace-nowrap" >
                                         {{ $link->platform }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        <div class="w-full p-2 text-left bg-white border border-gray-200 rounded-lg shadow sm:p-8">
-                                            <h5 class="mb-1 font-bold text-gray-900">{{ $link->title }}</h5>
-                                            <p class="mb-2 text-sm text-gray-500 sm:text-sm"> | @foreach ($link->tags as $tag ) {{ $tag->title . ' | ' }} @endforeach </p>
+                                        <div class="w-full p-2 text-left">
+                                            <h5 class="mb-1 text-xs font-bold text-gray-900 lg:text-base">{{ $link->title }}</h5>
+                                            <p class="mb-2 text-xs text-slate-500 lg:text-base"> @foreach ($link->tags as $tag ) {{ '#'.$tag->title.' ' }} @endforeach </p>
                                         </div>
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td class="px-6 py-4 text-xs lg:text-base">
                                         {{ date_format($link->created_at, "d-m-Y") }}
                                     </td>
                                     <td class="px-6 py-4 text-right flex items-center justify-center space-x-2 min-h-12">
                                         <div class="flex p-5 md:pt-8 lg:pt-12">
                                             <div class="flex flex-col justify-center space-y-4">
                                                 <a href="{{ $link->url }}" target="_blank">
-                                                    <svg class="flex-grow-0 flex-shrink-0 text-slate-400 w-5 h-5 mx-auto duration-300 hover:text-blue-800 hover:translate-x-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                                                    <svg class="flex-grow-0 flex-shrink-0 text-slate-400 w-3 h-3 md:w-5 md:h-5 mx-auto duration-300 hover:text-blue-800 hover:translate-x-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
                                                     </svg>
                                                 </a> 
                                                 <button wire:click="delete_link('{{ $link->id }}')">
-                                                    <svg class="flex-grow-0 flex-shrink-0 text-slate-400 w-5 h-5 mx-auto duration-300 hover:text-red-800 hover:scale-125" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <svg class="flex-grow-0 flex-shrink-0 text-slate-400 w-3 h-3 md:w-5 md:h-5 mx-auto duration-300 hover:text-red-800 hover:scale-125" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                                     </svg>
                                                 </button>
@@ -155,7 +156,13 @@
                     <div wire:loading.table role="status" class="ml-3 mb-3">
                         <span>Loading...</span>
                     </div> <!-- /. div wire:loading.table -->
-                    
+                    <!-- /. md, lg xl screen size table -->
+
+                    <!-- mobile table responsive -->
+                    <div class="block sm:hidden">
+                        here is going to be placed the mobile vertical table...
+                    </div>
+                    <!-- /. mobile table responsive -->
 
                 </div> <!-- /. class="relative overflow-x-auto shadow-md sm:rounded-lg" -->
             </div> <!-- /. class="bg-white overflow-hidden shadow-xl sm:rounded-lg" -->
