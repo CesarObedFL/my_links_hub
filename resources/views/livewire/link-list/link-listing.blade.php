bb3ebbc115e08b787b71c1e57b77f571873aa6eb
CesarObedFL <cesarobedfl@gmail.com>
2024-09-03 18:45:02 -0600

    [ADD] list-listing; mobile responsive v1.0

--- resources/views/livewire/link-list/link-listing.blade.php
+++ resources/views/livewire/link-list/link-listing.blade.php
@@ -19,9 +19,9 @@
                             <tr>
                                 <th colspan="1" scope="colgroup" class="px-2 py-3"> 
                                     <select wire:model.live="per_page"  class="block w-full p-2 mt-6 mb-6 text-sm text-slate-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
-                                        <option value="10">10 per page</option>
-                                        <option value="25">25 per page</option>
-                                        <option value="50">50 per page</option>
+                                        <option value="3">10 per page</option>
+                                        <option value="6">25 per page</option>
+                                        <option value="10">50 per page</option>
                                     </select>
                                 </th>
                                 <th colspan="4" scope="colgroup" class="px-6 py-3"> 
@@ -153,16 +153,55 @@
                         </tfoot>
                     </table>
 
+                    <!-- mobile table responsive -->
+                    <div class="block sm:hidden">
+                        <div class="grid grid-cols-1 gap-3">
+                            @foreach( $link_list as $link )
+                                <article class="bg-white h-200 w-200 flex flex-col shadow-lg rounded overflow-hidden">
+                                    <div class="h-25">
+                                        @if ( $link->thumbnail )
+                                            <img src="{{ asset('thumbnails/'.$link->thumbnail) }}" class="h-full w-full object-cover object-center" alt="{{ $link->thumbnail }}">
+                                        @else 
+                                            <img src="{{ asset('images/app/default_image.png') }}" class="h-full w-full object-cover object-center" alt="default image">
+                                        @endif
+                                    </div>
+                                    <div class="p-5 space-y-3 flex-1">
+                                        <h3 class="text-sm font-semibold text-slate-400">{{ $link->platform }}</h3>
+                                        <div class="w-full p-2 text-left">
+                                            <h5 class="mb-1 text-xs font-bold text-gray-900 lg:text-base">{{ $link->title }}</h5>
+                                            <p class="mb-2 text-xs text-slate-500 lg:text-base"> @foreach ($link->tags as $tag ) {{ '#'.$tag->title.' ' }} @endforeach </p>
+                                        </div>
+                                        <span>{{ date_format($link->created_at, "d-m-Y") }}</span>
+                                    </div>
+                                    <div class="flex justify-between p-5">
+                                        <a href="{{ $link->url }}" target="_blank">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-400 duration-300 hover:text-blue-800 hover:translate-x-3">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                                            </svg>
+                                        </a>
+                                        <button wire:click="delete_link('{{ $link->id }}')">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-400 duration-300 hover:text-red-800 hover:scale-125">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </article>
+                            @endforeach
+                            <div class="p-3 m-2">
+                                @if ( $link_list->count() )
+                                    {{ $link_list->links() }}
+                                @else
+                                    <p>There isn't results searching "{{ $search }}" in the page {{ $page }} showing {{ $per_page }} per page...</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /. mobile table responsive -->
+
                     <div wire:loading.table role="status" class="ml-3 mb-3">
                         <span>Loading...</span>
                     </div> <!-- /. div wire:loading.table -->
                     <!-- /. md, lg xl screen size table -->
-
-                    <!-- mobile table responsive -->
-                    <div class="block sm:hidden">
-                        here is going to be placed the mobile vertical table...
-                    </div>
-                    <!-- /. mobile table responsive -->
 
                 </div> <!-- /. class="relative overflow-x-auto shadow-md sm:rounded-lg" -->
             </div> <!-- /. class="bg-white overflow-hidden shadow-xl sm:rounded-lg" -->
