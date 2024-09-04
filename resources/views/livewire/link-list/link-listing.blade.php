f499e918f30dbe0f8df864b99d081a75d91045d2
CesarObedFL <cesarobedfl@gmail.com>
2024-09-03 19:49:54 -0600

    [ADD] list-listing; mobile responsive v2.0

--- resources/views/livewire/link-list/link-listing.blade.php
+++ resources/views/livewire/link-list/link-listing.blade.php
@@ -157,33 +157,35 @@
                     <div class="block sm:hidden">
                         <div class="grid grid-cols-1 gap-3">
                             @foreach( $link_list as $link )
-                                <article class="bg-white h-200 w-200 flex flex-col shadow-lg rounded overflow-hidden">
-                                    <div class="h-25">
+                                <article class="bg-white h-100 w-200 grid grid-cols-2 shadow-lg rounded overflow-hidden">
+                                    <div class="h-25 w-25">
                                         @if ( $link->thumbnail )
                                             <img src="{{ asset('thumbnails/'.$link->thumbnail) }}" class="h-full w-full object-cover object-center" alt="{{ $link->thumbnail }}">
                                         @else 
                                             <img src="{{ asset('images/app/default_image.png') }}" class="h-full w-full object-cover object-center" alt="default image">
                                         @endif
                                     </div>
-                                    <div class="p-5 space-y-3 flex-1">
-                                        <h3 class="text-sm font-semibold text-slate-400">{{ $link->platform }}</h3>
-                                        <div class="w-full p-2 text-left">
-                                            <h5 class="mb-1 text-xs font-bold text-gray-900 lg:text-base">{{ $link->title }}</h5>
-                                            <p class="mb-2 text-xs text-slate-500 lg:text-base"> @foreach ($link->tags as $tag ) {{ '#'.$tag->title.' ' }} @endforeach </p>
+                                    <div>
+                                        <div class="p-5 space-y-3 flex-1">
+                                            <h3 class="text-sm font-semibold text-slate-400">{{ $link->platform }}</h3>
+                                            <div class="w-full p-2 text-left">
+                                                <h5 class="mb-1 text-xs font-bold text-gray-900 lg:text-base">{{ $link->title }}</h5>
+                                                <p class="mb-2 text-xs text-slate-500 lg:text-base"> @foreach ($link->tags as $tag ) {{ '#'.$tag->title.' ' }} @endforeach </p>
+                                            </div>
+                                            <span>{{ date_format($link->created_at, "d-m-Y") }}</span>
                                         </div>
-                                        <span>{{ date_format($link->created_at, "d-m-Y") }}</span>
-                                    </div>
-                                    <div class="flex justify-between p-5">
-                                        <a href="{{ $link->url }}" target="_blank">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-400 duration-300 hover:text-blue-800 hover:translate-x-3">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
-                                            </svg>
-                                        </a>
-                                        <button wire:click="delete_link('{{ $link->id }}')">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-400 duration-300 hover:text-red-800 hover:scale-125">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                            </svg>
-                                        </button>
+                                        <div class="flex justify-between p-5">
+                                            <a href="{{ $link->url }}" target="_blank">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-400 duration-300 hover:text-blue-800 hover:translate-x-3">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                                                </svg>
+                                            </a>
+                                            <button wire:click="delete_link('{{ $link->id }}')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-400 duration-300 hover:text-red-800 hover:scale-125">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                            </button>
+                                        </div>
                                     </div>
                                 </article>
                             @endforeach
