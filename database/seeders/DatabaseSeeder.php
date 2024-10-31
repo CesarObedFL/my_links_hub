cd3784665b946de753e64bb866f0d9abcc648e0c
CesarObedFL <cesarobedfl@gmail.com>
2024-10-31 16:54:15 -0600

    [ADD] User Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'César Obed Figueroa Luna',
             'email' => 'test@example.com',
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
