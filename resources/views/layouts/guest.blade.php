5e98dffc86ab5660bb8a1be271777833c38b6d8f
CesarObedFL <cesarobedfl@gmail.com>
2025-01-10 13:49:45 -0600

    [FIX] removal footer from the guest view (login)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,10 +24,6 @@
             {{ $slot }}
         </div>
 
-        <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-        </footer>
-
         @livewireScripts
     </body>
 </html>
